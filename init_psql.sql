dd880fd440b35c47d5d4c07f26c4d9015416ccab
TopsterTX <bbs.100@yandex.ru>
2024-09-08 17:28:55 +0300

    [feature] add the init_psql file

---
+++ init_psql.sql
@@ -0,0 +1,25 @@
+create type meeting_status as enum ('creating','editing','cancel','reject','processing','expired');
+
+create table users (
+    id uuid primary key not null default gen_random_uuid(),
+    username varchar(255) not null,
+    chat_id int not null,
+    first_name varchar(255) not null
+);
+
+create table meetings (
+    id uuid primary key not null default gen_random_uuid(),
+    theme varchar(255),
+    status meeting_status not null default 'creating',
+    admin_user_id uuid,
+    date varchar(255),
+    foreign key (admin_user_id) references users(id)
+);
+
+create table user_meeting (
+    meeting_id uuid not null,
+    user_id uuid not null,
+    foreign key (meeting_id) references meetings(id),
+    foreign key (user_id) references users(id)
+);
+
