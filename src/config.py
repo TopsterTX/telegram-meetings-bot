1819bd7c925927d68402a59e176646b600107a7b
TopsterTX <bbs.100@yandex.ru>
2024-09-21 18:16:54 +0300

    [feature] remove the print

--- src/config.py
+++ src/config.py
@@ -9,5 +9,3 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
-
-print(BOT_TOKEN, DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT)
